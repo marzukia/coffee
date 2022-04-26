5f940ae0df83511e998675070c23157edc4ebee4
Andryo Marzuki <stabbish@gmail.com>
2022-04-26 22:58:38 +1200

    tweak conf

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../../coffee/"))
+sys.path.insert(0, os.path.abspath("../coffee/"))
 
 
 # Configuration file for the Sphinx documentation builder.
