fb3340672acbb73bc5bbe83efd2ee120e71d5972
Andryo Marzuki <stabbish@gmail.com>
2022-04-26 22:55:55 +1200

    fix init

--- coffee/__init__.py
+++ coffee/__init__.py
@@ -1,3 +1,8 @@
 # flake8: noqa
 from .views import *
 from .exceptions import *
+from .contrib import *
+from .management import *
+from .apps import *
+from .exceptions import *
+from .urls import *
